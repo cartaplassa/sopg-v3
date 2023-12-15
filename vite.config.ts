2c82ebaf181e16f2b42ee4a9b89b1cb5225a8cc2
zarni-ein <filippov@mail.ch>
2023-12-15 15:53:28 +0300

    Polishes: vite-tsconfig-paths plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@icons": path.resolve(__dirname, "./src/components/icons"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@store": path.resolve(__dirname, "./src/store"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-    },
-  },
-  plugins: [react()],
+  plugins: [react(), tsconfigPaths()],
   base: "/sopg-v3",
 });
